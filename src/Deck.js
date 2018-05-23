3decf42d0db915e006d26539fb1febc954d0187c
Zac McClung <zac.mcclung370@gmail.com>
2018-05-22 21:27:21 -0600

    deck data

---
+++ src/Deck.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+class Deck extends Component {
+  render() {
+    return <View>Hey there partner</View>;
+  }
+}
+export default Deck;
