3116af079340fb62ddc458d2eb55e03ba99f82bf
Zac McClung <zac.mcclung370@gmail.com>
2018-05-22 21:36:52 -0600

    interplay between components

--- App.js
+++ App.js
@@ -14,10 +14,14 @@ const DATA = [
 ];
 
 export default class App extends Component {
+  renderCard(item) {
+    return <Text>{item.text}</Text>;
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Deck />
+        <Deck data={DATA} renderCard={this.renderCard} />
       </View>
     );
   }
